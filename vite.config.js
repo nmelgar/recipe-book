3f4ea8fa58a0234cd0afcf0fca3de1c95369583e
nmelgar <nmelgar16013@gmail.com>
2024-10-24 09:49:27 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ export default defineConfig({
   root: 'src/',
 
   build: {
-    outDir: '../dist/client',
+    outDir: '../dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
+        allSuperheroes: resolve(__dirname, 'src/all-superheroes/index.html'),
+        heroDetails: resolve(__dirname, 'src/hero/index.html'),
       },
     },
   },
