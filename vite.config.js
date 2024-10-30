5ae80c7ac89a348ad303624c9dd1313638fc32dc
nmelgar <nmelgar16013@gmail.com>
2024-10-29 19:09:50 -0600

    Update vite and files to display notes on deployment.

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         allSuperheroes: resolve(__dirname, 'src/all-superheroes/index.html'),
         heroDetails: resolve(__dirname, 'src/hero/index.html'),
+        notes: resolve(__dirname, 'src/notes/index.html'),
       },
     },
   },
